91c1a31b6b1890d96060dff55da10f3079a4342f
joshuahockey <85370003+joshuahockey@users.noreply.github.com>
2022-10-13 10:55:08 -0700

    added css and html id for confetti

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 // My Scripts
 
 console.log('Page Loaded');
+
+let confetti = new Confetti("confetti");
+
+confetti.setCount(10);
+confetti.setSize(10);
+confetti.setPower(25);
+confetti.setFade(false);
+confetti.destroyTarget(false);
